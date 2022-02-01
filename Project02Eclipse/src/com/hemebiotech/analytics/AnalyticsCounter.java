ba9accc144d13bd90dfa5f45fba2936813371c84
S060980 <S060980@PO702956.siege.axa-fr.intraxa>
2022-02-01 10:58:40 +0100

    Etape 3 : Refactoring de la classe AnalyticsCounter, avec suppression des classes inutiles du projet initial (ISymptomReader.java et ReadSymptomDataFromFile.java) et suppression commentaires

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -16,15 +16,13 @@ public class AnalyticsCounter {
 	List<String> symptomList = new ArrayList<String>();
 	Map<String, Integer> noSortedSymptomList = new HashMap<String, Integer>();
 
-	// ETAPE 1 - Lecture Fichier et stockage valeurs symptomes avec un objet
-	// ArrayList
 	public void readFile(String filePathInput) {
 
 		if (filePathInput != null) {
 			try {
 				BufferedReader reader = new BufferedReader(new FileReader(filePathInput));
 				String line = reader.readLine();
-				// Boucle sur chaque ligne du fichier
+
 				while (line != null) {
 					symptomList.add(line);
 					line = reader.readLine();
@@ -36,44 +34,34 @@ public class AnalyticsCounter {
 		}
 	}
 
-	// ETAPE 2 - Lecture contenu ArrayList (boucle) et sauvegarde dans un
-	// dictionnaire HashMap
-	// => Un Symptome = 1 clé unique. Pour chaque clé sera renseigné le nombre
-	// d'occurence lu dans l'Arraylist
 	public void countOcc() {
 
 		for (int i = 0; i < symptomList.size(); i++) {
 			String symptom = symptomList.get(i);
-			// vérification pre-existance du symptome dans la HashMap
+
 			boolean exists = noSortedSymptomList.containsKey(symptom);
-			// Cas du symptome deja existant dans le dictionnaire
-			// => Ajouter 1 à la valeur
+
 			if (exists == true) {
 				int nbOcc = noSortedSymptomList.get(symptom) + 1;
 				noSortedSymptomList.put(symptom, nbOcc);
-			}
-			// sinon le créer et mettre une valeur de "1"
-			else {
+			} else {
 				noSortedSymptomList.put(symptom, 1);
 			}
 		}
-		System.out.println("noSortedSymptomList =>" + noSortedSymptomList);
+
 	}
 
-	// ETAPE 3 - TRI DICTIONNAIRE SYMPTOMES ET ECRITURE FICHIER SORTIE.
-	// Utilisation de la classe TreeMap
 	public void writeFile(String filePathOutput) {
 
 		Map<String, Integer> SortedSymptomList = new TreeMap<String, Integer>(noSortedSymptomList);
 
 		try {
 			BufferedWriter writer = new BufferedWriter(new FileWriter(filePathOutput));
-			// Boucle sur TreeMap pour Récupérer les valeurs et les clés
+
 			for (Map.Entry<String, Integer> entry : SortedSymptomList.entrySet()) {
 				String key = entry.getKey();
 				Integer value = entry.getValue();
 				String keyValue = key + " : " + value;
-				System.out.println(keyValue);
 				writer.write(keyValue);
 				writer.newLine();
 			}
